30a1385862679ad0ae4a96a2e5b28a4109627c91
unknown <ypf12pxu@CMPLAB1-03.cmp.uea.ac.uk>
2016-01-12 18:12:21 +0000

    activity now should add event to calender, only test data

--- app/src/main/java/com/activity/adam/activityprogram/ActivityDetails.java
+++ app/src/main/java/com/activity/adam/activityprogram/ActivityDetails.java
@@ -1,11 +1,15 @@
 package com.activity.adam.activityprogram;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.CalendarContract;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+
+import java.util.Calendar;
 
 public class ActivityDetails extends AppCompatActivity {
 
@@ -20,8 +24,20 @@ public class ActivityDetails extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Calendar beginTime = Calendar.getInstance();
+                beginTime.set(2012, 0, 19, 7, 30);
+                Calendar endTime = Calendar.getInstance();
+                endTime.set(2012, 0, 19, 8, 30);
+                Intent intent = new Intent(Intent.ACTION_INSERT)
+                        .setData(CalendarContract.Events.CONTENT_URI)
+                        .putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME, beginTime.getTimeInMillis())
+                        .putExtra(CalendarContract.EXTRA_EVENT_END_TIME, endTime.getTimeInMillis())
+                        .putExtra(CalendarContract.Events.TITLE, "Yoga")
+                        .putExtra(CalendarContract.Events.DESCRIPTION, "Group class")
+                        .putExtra(CalendarContract.Events.EVENT_LOCATION, "The gym")
+                        .putExtra(CalendarContract.Events.AVAILABILITY, CalendarContract.Events.AVAILABILITY_BUSY)
+                        .putExtra(Intent.EXTRA_EMAIL, "rowan@example.com,trevor@example.com");
+                startActivity(intent);
             }
         });
     }
